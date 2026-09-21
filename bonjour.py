e47961eb5f8125a7dc21ced3b27586048fdbc77c
hugo-gbotte <hugogbotte93@gmail.com>
2026-09-21 18:38:06 +0000

    Add initial Python program for personal introduction

---
+++ bonjour.py
@@ -0,0 +1,12 @@
+# Mon premier programme - Hugo Gbotte
+# Etudiant à Shenyang, Objectif Canada 2029
+
+nom = "Hugo"
+pays_actuel = "Chine"
+pays_objectif = "Canada"
+annee = 2026
+
+print(f"Salut ! Moi c'est {nom} 👋")
+print(f"Je suis en {pays_actuel} en {annee}")
+print(f"Mon objectif : devenir développeur au {pays_objectif} en 2029 🇨🇦")
+print("Ce code est sur mon GitHub !")
